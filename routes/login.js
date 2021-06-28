27dee6563c9360a68c0ae0c410a47d9d9b852ff7
Maxwell Wehner <maxwehnerwork@gmail.com>
2021-06-28 14:40:35 -0700

    basic implentation for login

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const { asyncHandler, csrfProtection } = require("./utils");
+const db = require("../db/models/");
+const bcrypt = require("bcryptjs");
 
-/* GET home page. */
 router.get(
   "/",
   csrfProtection,
-  asyncHandler(async (req, res, next) => {
+  asyncHandler(async (req, res) => {
     const email = null;
-    res.render("login", { title: "Login-Yoda-Flow", email });
+    res.render("login", {
+      title: "Login-Yoda-Flow",
+      email,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
+
+router.post(
+  "/",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await db.User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (user !== null) {
+      const passwordMatched = await bcrypt.compare(
+        password,
+        user.hashedPassword.toString()
+      );
+      if (passwordMatched) {
+        //add logIn user
+        return res.redirect("/");
+      }
+    }
   })
 );
 
