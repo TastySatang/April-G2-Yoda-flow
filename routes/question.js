3a518c62bdb43cb81d9c09d054686a6aa6f0b9e4
tan004 <qq563169939@yahoo.com>
2021-06-29 12:55:53 -0700

    authorized user can submit a new question.

--- routes/question.js
+++ routes/question.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require("express-validator")
-const { loginUser } = require("../auth")
+const { loginUser, requireAuth } = require("../auth")
 const db = require("../db/models/");
 const { asyncHandler, csrfProtection } = require("./utils");
 
@@ -17,10 +17,24 @@ router.get('/', asyncHandler(async (req, res) => {
 
 }));
 
-router.get('/new', csrfProtection, asyncHandler(async (req, res) => {
+
+router.get('/new', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
     res.render('new-question', {
-        title: "New Question Yoda Flow"
+        title: "New Question Yoda Flow",
+        csrfToken: req.csrfToken()
     });
+}));
+
+router.post('/new', requireAuth, csrfProtection, asyncHandler(async(req,res)=>{
+    const { content } = req.body;
+
+    const question = await db.Question.build({
+        userId: res.locals.user.id,
+        content,
+    });
+
+    await question.save();
+    req.session.save(() => res.redirect('/questions'));
 }))
 
 
