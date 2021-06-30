ccf4cbc317593d2cee059f9cb3292a1f7bee91d8
tan004 <qq563169939@yahoo.com>
2021-06-30 11:09:01 -0700

    working on post the answer

--- routes/answer.js
+++ routes/answer.js
@@ -7,13 +7,23 @@ const { loginUser, requireAuth } = require("../auth")
 const { questionId } =require('./question')
 
 
-router.get('/question/:questionId(\\d+)/answer/new', csrfProtection, requireAuth, asyncHandler(async(req, res, next)=> {
+router.get('/questions/:questionId(\\d+)/answer/new', csrfProtection, requireAuth, asyncHandler(async(req, res, next)=> {
+    const questionId = req.params.questionId;
+    const question = await db.Question.findByPk(questionId);
 
     res.render('new-answer', {
         title: "Answer-Yoda-Flow",
         csrfToken: req.csrfToken(),
+        question
     })
 
 }));
+
+router.post(`/questions/:quesitonId(\\d+)/answer/new`, csrfProtection, asyncHandler(async(req, res)=>{
+    const questionId = req.params.questionId;
+    const question = await db.Question.findByPk(questionId);
+
+
+}))
 
 module.exports = router;
